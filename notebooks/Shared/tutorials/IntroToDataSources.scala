de164bd03773f44b26ec9442528cab5e4b32ce9a
Davide Mauri <info@davidemauri.it>
2018-03-20 11:25:11 +0800

    Improved comments and description

--- notebooks/Shared/tutorials/IntroToDataSources.scala
+++ notebooks/Shared/tutorials/IntroToDataSources.scala
@@ -44,12 +44,34 @@
 
 // COMMAND ----------
 
+// MAGIC %md
+// MAGIC ## Moving data from CSV to SQL Azure
+// MAGIC In the next paragraph you're going to extracting data from a CSV file stored in an Azure Blob Store, do some basic queries using Spark SQL and then save the loaded data into an Azure SQL database. 
+
+// COMMAND ----------
+
+// MAGIC %md 
+// MAGIC Configure spark to access an existing Azure Blob store where the CSV file that we want to import is stored
+
+// COMMAND ----------
+
 spark.conf.set("fs.azure.account.key.openhackspark.blob.core.windows.net",  "xlkvzaPoN5MQvYgT/Yg70s6sEw2KBkrLpiqhbrR9IhHC8gbvP41MeMGjuljPpsAjvCzUn3MIjSaQ/w8oXDoroQ==")
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC In order to manipulate data using Spark SQL, specialized function and types needs to be imported
 
 // COMMAND ----------
 
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Create a schema for the CSV file so that data can be manipulated more easily and also checked for inconsistencies. The approach of creating the schema *after* having loaded data somewhere, it is called **schema-on-read** as opposed to the **schema-on-write** approach. **Schema-on-write** more suitable where youknow in advance the shape of your data, and you want to make sure that only the data compliant with the schema is loaded.
+// MAGIC In an IoT scenario, **the schema-on-read** is usually preferred since it gives more flexbility and favor the idea of storing the data even if we don't know how to deal with it just yet. Think, for example, at the case of adding a shiny new sensor to a set of existing one. The new sensor may return additional data that you don't want to miss, even if your application is not yet ready to deal with it, but it will be in future. 
 
 // COMMAND ----------
 
@@ -66,9 +88,19 @@ val schema = StructType(
 
 // COMMAND ----------
 
+// MAGIC %md 
+// MAGIC Read the file. The file is the famous Iris Dataset taken from the https://archive.ics.uci.edu/ml/datasets/Iris. If you want to start exploring Machine Learning, this is a great dataset to get get started.
+
+// COMMAND ----------
+
 // file originally from 
 // https://archive.ics.uci.edu/ml/datasets/iris
 val irisDF = sqlContext.read.schema(schema).format("csv").load("wasb://sample-data@openhackspark.blob.core.windows.net/iris.data")
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Show first 10 lines using the *take* method on the created DataFrame
 
 // COMMAND ----------
 
@@ -77,7 +109,17 @@ irisDF.take(10)
 
 // COMMAND ----------
 
+// MAGIC %md
+// MAGIC Create a temporary view so that we can manipulate data using standard SQL commands, that will make data manipulation much easier
+
+// COMMAND ----------
+
 irisDF.createOrReplaceTempView("iris")
+
+// COMMAND ----------
+
+// MAGIC %md 
+// MAGIC Execute a very simple SQL query on created view. Spark SQL supports ANSI SQL:2003 that allows really complexy data manipulation, perfect for data science needs (https://en.wikipedia.org/wiki/SQL:2003)
 
 // COMMAND ----------
 
@@ -92,6 +134,11 @@ irisDF.createOrReplaceTempView("iris")
 // COMMAND ----------
 
 spark.sql("select * from iris limit 10").show(truncate=false)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Connect to an Azure SQL database using JDBC driver
 
 // COMMAND ----------
 
@@ -116,9 +163,19 @@ connectionProperties.setProperty("Driver", driverClass)
 
 // COMMAND ----------
 
+// MAGIC %md
+// MAGIC Execute a command to check that connection is working properly
+
+// COMMAND ----------
+
 // Let's check if connection with Azure SQL is up and running
 // (https://docs.azuredatabricks.net/spark/latest/data-sources/sql-databases.html#push-down-a-query-to-the-database-engine)
 val serverName = spark.read.jdbc(jdbcUrl, "(select @@servername as ServerName) t", connectionProperties)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC View the result: it should be the name of the Azure SQL server you are connected to
 
 // COMMAND ----------
 
@@ -126,11 +183,16 @@ display(serverName)
 
 // COMMAND ----------
 
+// MAGIC %md
+// MAGIC Copy the Iris Spark table content to Azure SQL. Depending on the specificed *mode* option, target table can be created automatically or not.
+// MAGIC - https://docs.azuredatabricks.net/spark/latest/data-sources/sql-databases.html#write-data-to-jdbc
+// MAGIC - https://spark.apache.org/docs/2.3.0/api/scala/index.html#org.apache.spark.sql.DataFrameWriter
+
+// COMMAND ----------
+
 import org.apache.spark.sql.SaveMode
 
 // Drop existing table if needed, create a new table and fill it 
-// https://docs.azuredatabricks.net/spark/latest/data-sources/sql-databases.html#write-data-to-jdbc
-// https://spark.apache.org/docs/2.3.0/api/scala/index.html#org.apache.spark.sql.DataFrameWriter
 spark.sql("select * from iris")
      .write
      .mode(SaveMode.Overwrite)
@@ -138,3 +200,9 @@ spark.sql("select * from iris")
 
 // COMMAND ----------
 
+// MAGIC %md
+// MAGIC Done! Let's check table content, by reading back the table on Azure SQL
+
+// COMMAND ----------
+
+spark.read.jdbc(jdbcUrl, "dbo.Iris", connectionProperties).show()
