c1e826a591c1741a5f39311fef809c20a2827662
David Makogon <dmakogon@gmail.com>
2018-03-22 14:20:43 +0800

    Top-level notebook, serving as launch-point to additional notebooks.

---
+++ notebooks/Shared/tutorials/StartHere.scala
@@ -0,0 +1,12 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # Welcome!
+// MAGIC 
+// MAGIC This set of notebooks provide short introductions to several key concepts when working with Spark+Azure, with Databricks. Where applicable, these notebooks also contain links to additional resources.
+// MAGIC 
+// MAGIC  - Databricks notebooks [[link]($./IntroToNotebooks)]
+// MAGIC  - Event Hubs / IoT Hub Integration with Spark [[link]($./IntroToEventHubs)]
+// MAGIC  - Spark core concepts: RDDs, Dataframes, SQL [[link]($./IntroToDataframes)]
+// MAGIC  - Data sources: Azure Storage, Azure SQL Database [[link]($./IntroToDataSources)]
+// MAGIC  
+// MAGIC Additionally, [[this notebook]($./WordcountExample)] presents a very simple "word count" example using Spark.
